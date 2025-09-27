2a8a804a61a7a6521e945a793cd2e04b922637f5
Johan Wigmo <johanwigmo@gmail.com>
2025-09-27 22:50:01 +0200

    Create Xcode project

---
+++ TodoTxt/TodoTxtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTxtApp.swift
+//  TodoTxt
+//
+//  Created by Johan Wigmo on 2025-09-27.
+//
+
+import SwiftUI
+
+@main
+struct TodoTxtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
